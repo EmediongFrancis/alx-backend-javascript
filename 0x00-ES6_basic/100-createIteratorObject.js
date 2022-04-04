ca41706cba1ef0af985863acf72ba419993d462e
Emediong Francis <emediongfrancis@gmail.com>
2022-04-04 21:03:32 +0100

    Create iterator object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,6 @@
 export default function createIteratorObject(report) {
   const Employee = [];
+  // eslint-disable-next-line no-unused-vars
   for (const [departments, employees] of Object.entries(report.allEmployees)) {
     for (const employee of employees) {
       Employee.push(employee);
