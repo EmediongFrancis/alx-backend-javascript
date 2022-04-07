d55b18be4f42fb83146933ef4fef306f8cebfb10
Emediong ‘Bendito’ Francis <emediongfrancis@gmail.com>
2022-04-07 15:05:18 +0100

    Fix typo

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -6,7 +6,7 @@ export default async function asyncUploadUser() {
       photo: await uploadPhoto(),
       user: await createUser(),
     };
-  } catch (err) {
+  } catch (error) {
     return {
       photo: null,
       user: null,
