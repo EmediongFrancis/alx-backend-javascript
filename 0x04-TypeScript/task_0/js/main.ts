6ef00453e128a7f3dc70d17178ea64dc291a3feb
Emediong ‘Bendito’ Francis <emediongfrancis@gmail.com>
2022-04-13 14:02:26 +0100

    Create Student interface

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Bendito',
+    lastName: 'Francis',
+    age: 69,
+    location: 'Lagos',
+}
+
+const student2: Student = {
+    firstName: 'Amoroso',
+    lastName: 'Akpanyung',
+    age: 96,
+    location: 'Lagos',
+}
+
+const studentsList: Array<Student> = [student1, student2];
