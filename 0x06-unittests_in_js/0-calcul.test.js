4efc9f22f36a2205dbff988eac1cc2183b5941b0
Emediong Francis <emediongfrancis@gmail.com>
2022-06-08 14:30:54 +0100

    Run basic test with assert lib

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -12,6 +12,16 @@ describe("Tests calculateNumber function:", function() {
             assert.strictEqual(calculateNumber(1, 3.7), 5);
         });
     });
+    describe("Calculates one float & one integer (round down):", function() {
+        it('Returns 4.', function() {
+            assert.strictEqual(calculateNumber(1, 3.3), 4);
+        });
+    });
+    describe("Calculates one float & one int reversed:", function () {
+    it('Returns 5.', function () {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
+  });
     describe("Calculates two floats:", function() {
         it('Returns 5.', function() {
             assert.strictEqual(calculateNumber(1.2, 3.7), 5);
